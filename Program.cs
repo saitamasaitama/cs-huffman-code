d48f19c9fe2c4efabdacfe8d47f20e8f79f28d10
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2020-11-06 16:32:07 +0900

    クラスライブラリに変更

--- Program.cs
+++ Program.cs
@@ -1,61 +1,19 @@
 ﻿using System;
 using System.Linq;
 using System.IO;
-using System.Buffers;
+
 using System.Collections;
 using System.Collections.Generic;
 
-namespace cs_huffman_code
+
+public class HuffmanCode
 {
-  class Program
+  public static List<T> Decode<T>(HuffmanData<T> data)
   {
-    static void Main(string[] args)
-    {
-      HuffmanData<int> result=(
-      HuffmanCode.Encode<int>(new int[]{
-        111,222,333,
-        222,333,
-        333,
-          }));
-      Console.Write($"LENGTH={result.ToBytes().Length}Byte\n");
-     
-      List<int> decode= HuffmanCode.Decode<int>(result);
-      foreach (bool b in result.body)
-      {
-        Console.Write($"{(b?1:0)},");
-      }
-      Console.WriteLine();
-
-      foreach (KeyValuePair<int,bool[]> kv in result.header)
-      {
-        Console.WriteLine($"{kv.Key}:");
-        foreach(bool b in kv.Value)
-        {
-          Console.Write($"{(b ? 1 : 0)},");
-        }
-        Console.WriteLine();
-      }
-
-      Console.WriteLine();
-
-      
-      foreach (int byt in decode)
-      {
-        Console.Write($"{byt},");
-      }
-      
-    }
-  }
-}
-
-public class HuffmanCode {
-
-  public static List<T> Decode<T>(HuffmanData<T> data){
     var result = new List<T>();
-
     int count = 0;
     //dataから読み込み
-    foreach(bool b in data.body)
+    foreach (bool b in data.body)
     {
       if (b)
       {
@@ -70,46 +28,50 @@ public class HuffmanCode {
     return result;
   }
 
-  public static HuffmanData<T> Encode<T>(T[] source){
-    var count=new Dictionary<T,int>();
+  public static HuffmanData<T> Encode<T>(T[] source)
+  {
+    var count = new Dictionary<T, int>();
 
     //カウント処理
-    foreach(T data in source ){
-      if(!count.ContainsKey(data)){
+    foreach (T data in source)
+    {
+      if (!count.ContainsKey(data))
+      {
         count[data] = 0;
       }
       count[data]++;
     }
 
+    var list = count.OrderByDescending(v => v.Value)
+      .Select(v => v.Key).ToList();
+    var table = new Dictionary<T, bool[]>();
 
-    var list= count.OrderByDescending(v=>v.Value)
-      .Select(v=>v.Key).ToList();
-    var table=new Dictionary<T,bool[]>();
-    
     //テーブルを生成
-    for(int i=0;i<list.Count();i++){
-      
+    for (int i = 0; i < list.Count(); i++)
+    {
+
       table.Add(list[i], createHuffman(i));
     }
-    var body=new HuffmanCodeBody();
+    var body = new HuffmanCodeBody();
 
     //符号化処理
-    foreach(T data in source){
+    foreach (T data in source)
+    {
       body.AddRange(table[data]);
     }
 
-    return new HuffmanData<T>(){
-      header=table,
-      indexTable=list,
-      body=body
+    return new HuffmanData<T>()
+    {
+      header = table,
+      indexTable = list,
+      body = body
     };
   }
 
   private static bool[] createHuffman(int num)
   {
     var result = new List<bool>();
-    
-    for(int i = 0; i < num; i++)
+    for (int i = 0; i < num; i++)
     {
       result.Add(false);
     }
@@ -118,43 +80,48 @@ public class HuffmanCode {
   }
 }
 
-public class HuffmanData<T>{
+public class HuffmanData<T>
+{
   public List<T> indexTable;
-  public Dictionary<T,bool[]> header;//ヘッダ
+  public Dictionary<T, bool[]> header;//ヘッダ
   public HuffmanCodeBody body;//実質的なバイナリ配列
 
-  public Byte[] ToBytes() {
-    var bits= new BitArray(body.ToArray());
+  public Byte[] ToBytes()
+  {
+    var bits = new BitArray(body.ToArray());
     var memory = new MemoryStream();
 
-    using (BinaryWriter writer=new BinaryWriter(memory))
+    using (BinaryWriter writer = new BinaryWriter(memory))
     {
-      for(int byteCount=0;byteCount < (bits.Count/8) + 1; byteCount++)
+      for (int byteCount = 0; byteCount < (bits.Count / 8) + 1; byteCount++)
       {
         Byte b = 0;
         //1を書き込み
-        for(int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++)
         {
           int index = (byteCount * 8 + i);
           if (bits.Count <= index) break;
           //オーバーするなら終了
           if (!bits[index]) continue;
-          b =(byte)(b|(1<<i));
+          b = (byte)(b | (1 << i));
         }
         writer.Write(b);
       }
-     
+
       return memory.ToArray();
     }
   }
 }
 
 
-public class HuffmanCodeBody:List<bool>{
-  public override string ToString(){
-    string s="";
-    foreach(bool b in this ){
-      s+=(b?"T":"F");
+public class HuffmanCodeBody : List<bool>
+{
+  public override string ToString()
+  {
+    string s = "";
+    foreach (bool b in this)
+    {
+      s += (b ? 1 : 0);
     }
     return s;
   }
